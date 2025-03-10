7211f1d0f301666ebf5e63022fcd75a3f85d8c28
Jonas Gertz <jonas.gertz@epicalgroup.com>
2025-03-11 00:40:33 +0100

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -44,34 +43,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
-<head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="preload"
-          href="/images/hero-image.jpg"
-          as="image"
-          type="image/jpeg"
-          crossOrigin="anonymous" // Correct property name
-        />
       </head>
       <body className={`min-h-screen themed-bg-pattern text-white`}>
         <div className="bg-black/40 min-h-screen ">
